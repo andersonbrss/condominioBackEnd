fc274307264a738a4b51c00138fc8ed3371103ad
Bruno <andersonss@ymail.com>
2020-03-09 13:26:10 -0300

    primeiro commit

---
+++ src/main/java/com/zends/condominio/comum/service/AbstractService.java
@@ -0,0 +1,19 @@
+package com.zends.condominio.comum.service;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+public interface AbstractService< T > {
+
+	ResponseEntity< ? > getObj( Long idObj );
+	
+	ResponseEntity< List< T >> getList();
+	
+	ResponseEntity< ? > save( T obj );
+	
+	ResponseEntity< ? > update( T obj );
+	
+	ResponseEntity< String > delete( Long idObj );
+	
+}
